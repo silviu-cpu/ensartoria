3ab297ef72453858264eca2875fb9de104d84382
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2020-11-15 20:11:37 +0200

    builded

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaPhone } from 'react-icons/fa'
 import Video from '../../videos/video.mp4'
-import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroTwoContent} from './HeroElements'
+import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1} from './HeroElements'
 const HeroSection = () => {
     return (
         
@@ -14,11 +14,8 @@ const HeroSection = () => {
             <HeroContent>
                 <HeroH1>Sartoria veloce, fatto su misura, ricamo</HeroH1>
                 <HeroH1>B2B Servizi</HeroH1>
-            </HeroContent>
-
-            {/* <HeroTwoContent>
                 <HeroH1><FaPhone /> +39 3533306491</HeroH1>
-            </HeroTwoContent> */}
+            </HeroContent>
 
         </HeroContainer>
         
